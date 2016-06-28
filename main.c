9dba5b579243dd2e778e8fde94aaa03318bcd2ef
franrevoredo <francisco.r.89@gmail.com>
2016-06-28 08:14:14 -0300

    Agregado Soporte para IPV6

--- main.c
+++ main.c
@@ -19,8 +19,9 @@ int main(int argc, char *const *argv) {
     int sd, sd_conn, c, thread_num = 2, port_num = 5000;
     int reuse = 1;
     socklen_t addrlen;
-    struct sockaddr_in srv_addr;
-    struct sockaddr_in cli_addr;
+    struct sockaddr_in6 srv_addr;
+    struct sockaddr_in6 cli_addr;
+char client_addr_ipv6[100];
 
 while ((c = getopt (argc, argv, "t:p:")) != -1)
     switch (c)
@@ -50,7 +51,7 @@ printf("\nEl nro de threads es: %d \n", thread_num);
 printf("\nEl nro de puerto es: %d \n", port_num);
 
 
- sd = socket(AF_INET, SOCK_STREAM, 0);
+ sd = socket(AF_INET6, SOCK_STREAM, 0);
 
     if (sd < 0) {
         perror("Error al abrir el SOCKET");
@@ -59,9 +60,10 @@ printf("\nEl nro de puerto es: %d \n", port_num);
     if (setsockopt(sd, SOL_SOCKET, SO_REUSEADDR, (const char *) &reuse, sizeof (reuse)) < 0)
         perror("setsockopt(SO_REUSEADDR) error");
 
-    srv_addr.sin_family = AF_INET;
-    srv_addr.sin_addr.s_addr = INADDR_ANY;
-    srv_addr.sin_port = htons(port_num);
+    srv_addr.sin6_flowinfo = 0;
+    srv_addr.sin6_family = AF_INET6;
+    srv_addr.sin6_addr = in6addr_any;
+    srv_addr.sin6_port = htons(port_num);
 
     if (bind(sd, (struct sockaddr *) &srv_addr, sizeof (srv_addr)) == -1) {
         perror("Error en el BIND");
@@ -70,6 +72,9 @@ printf("\nEl nro de puerto es: %d \n", port_num);
     if (listen(sd, 5) < 0) {
         perror("Error en listen.");
         exit(EXIT_FAILURE); }
+
+	addrlen = sizeof(cli_addr);
+
 
     while ((sd_conn = accept(sd, (struct sockaddr *) &cli_addr, &addrlen)) > 0) {
         switch (fork()) {
@@ -82,6 +87,11 @@ printf("\nEl nro de puerto es: %d \n", port_num);
                 break;
 
             default: // padre
+	if(sd_conn < 0) {
+		perror("Error en el Accept");
+		exit(EXIT_FAILURE); }
+	inet_ntop(AF_INET6, &(cli_addr.sin6_addr),client_addr_ipv6, 100);
+    printf("Incoming connection from client having IPv6 address: %s\n",client_addr_ipv6);
                 close(sd_conn);
                 break;
         }
